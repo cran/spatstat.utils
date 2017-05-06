ba3f8a95eb34af43a70f071b94b2f46e607cd1de
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2017-05-06 04:13:51 +0000

    version 1.6-0

--- src/proto.h
+++ src/proto.h
@@ -23,6 +23,12 @@ void Cmatchxy(int *, double *, double *, int *, double *, double *, int *);
 void inxyp(double *, double *, double *, double *, int *, int *, int *, int *);
 void prdist2segs(double *, double *, int *, double *, double *, double *, double *, int *, double *, double *); 
 void nndist2segs(double *, double *, int *, double *, double *, double *, double *, int *, double *, double *, int *);
+
+void CUmatch2int(int *, int *, int *, int *, int *, int *, int *); 
+void CSmatch2int(int *, int *, int *, int *, int *, int *, int *); 
+void CUmatch3int(int *, int *, int *, int *, int *, int *, int *, int *, int *); 
+void CSmatch3int(int *, int *, int *, int *, int *, int *, int *, int *, int *); 
+
 /*
 
              Functions invoked by .Call
